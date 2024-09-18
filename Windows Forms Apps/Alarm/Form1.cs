580fe3439782620f9dc73eb1958c20264a8bd77c
AH-CSharpEditor <alext73736@gmail.com>
2024-09-18 22:02:05 +0800

    倒數計時器功能調整 | Timer Functionality Updates
    + 新增「延後提醒」按鈕 | Add "Snooze" button & Implement feature
    + 最短提醒時間修改為「15秒後」 | Adjust MinDate duration to 15 seconds
    
    延後提醒功能說明：
    + 倒數結束後，可按下「延後提醒」按鈕
    + 系統將在1分鐘後重新提醒
    
    Snooze feature details:
    + Available after initial countdown ends
    + Pressing "Snooze" sets a new 1-minute reminder

--- Windows Forms Apps/Alarm/Form1.cs
+++ Windows Forms Apps/Alarm/Form1.cs
@@ -12,19 +12,25 @@ namespace Alarm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dateTimePicker1.MinDate = DateTime.Now.AddMinutes(1);
+            dateTimePicker1.MinDate = DateTime.Now.AddSeconds(15);
             dateTimePicker1.MaxDate = DateTime.Today.AddDays(7);
             dateTimePicker1.ShowUpDown = true;
+
+            button1Set.Enabled = true;
             bt2Reset.Enabled = false;
+            bt3Snooze.Enabled = false;
+
             timer1.Enabled = false;
-            timer1.Interval = 1000;   // 啟動後每秒判斷一次
-            timer2.Enabled = true; //現在時間計時器（常開）
+            timer1.Interval = 1000;     // 啟動後每1秒判斷一次
+            timer2.Enabled = true;      // 現在時間計時器（常開）
             timer2.Interval = 1000;
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
             bt2Reset.Enabled = true;
+            bt3Snooze.Enabled = false;
+            bt3Snooze.ForeColor = Color.DarkGray;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -38,15 +44,22 @@ namespace Alarm
             // 避免使用者重複選取
             button1Set.Enabled = false;
             bt2Reset.Enabled = false;
+            bt3Snooze.Enabled = false;
+            bt3Snooze.ForeColor = Color.DarkGray;
             dateTimePicker1.Enabled = false;
             textBox1.Enabled = false;
         }
 
         private void bt2Reset_Click(object sender, EventArgs e)
         {
-            dateTimePicker1.Value = DateTime.Now.AddMinutes(1);
+            dateTimePicker1.Value = DateTime.Now.AddSeconds(15);
             textBox1.Clear();
+            button1Set.Enabled = true;
             bt2Reset.Enabled = false;
+            bt3Snooze.Enabled = false;
+            bt3Snooze.ForeColor = Color.DarkGray;
+            dateTimePicker1.Enabled = true;
+            textBox1.Enabled = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -57,8 +70,10 @@ namespace Alarm
                 System.Media.SystemSounds.Beep.Play();
                 MessageBox.Show($"{textBox1.Text}", "Time's UP", MessageBoxButtons.OK);
                 dateTimePicker1.Value = DateTime.Now.AddMinutes(1);
-                dateTimePicker1.Enabled = true;
+                bt2Reset.Enabled = true;
                 textBox1.Enabled = true;
+                bt3Snooze.Enabled = true;
+                bt3Snooze.ForeColor = Color.Black;
             }
         }
 
@@ -66,6 +81,18 @@ namespace Alarm
         {
             timeNow = DateTime.Now.ToString("yyyy年MM月dd日(ddd) HH:mm:ss");
             label3TimeNow.Text = $"現在時間：\t{timeNow}";
+        }
+
+        private void bt3Postpone_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Now.AddMinutes(1);
+            dateTimePicker1.Enabled = false;
+            textBox1.Enabled = false;
+            button1Set.Enabled = false;
+            bt2Reset.Enabled = false;
+            bt3Snooze.Enabled = false;
+            bt3Snooze.ForeColor = Color.DarkGray;
+            timer1.Enabled = true;
         }
     }
 }
