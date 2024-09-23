0f58ac34ba622dbc7d13694dbcb07d9c3a40061f
AH-CSharpEditor <alext73736@gmail.com>
2024-09-23 21:14:30 +0800

    新增三項不同排序法練習專案｜Add 3 Sorting Algorithm Practice Projects
    - 選擇排序法｜Selection Sort
    - 插入排序法｜Insertion Sort
    - 氣泡排序法｜Bubble Sort

---
+++ Console Apps/BubbleSortPresentation/BubbleSort.cs
@@ -0,0 +1,67 @@
+﻿namespace BubbleSortPresentation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int ArraySize = 5;
+
+        int[] oriArray = new int[ArraySize];
+        Console.WriteLine($"Input {ArraySize} numbers:");
+        
+        // 使用者輸入原始陣列資料(int)
+        for(int i = 0; i < ArraySize;)
+        {
+            // 限制使用者僅能輸入整數數字
+            if(int.TryParse(Console.ReadLine(),out int k))
+            {
+                oriArray[i] = k;
+                i++;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
+        Console.WriteLine();
+        ShowArray(oriArray, "Original Array");  // 顯示原始資料
+
+        int[] sortArray = new int[ArraySize];
+        Array.Copy(oriArray, sortArray, ArraySize); // 複製一陣列用於BubbleSort
+
+        BubbleSort(ref sortArray);  // 氣泡排序法
+
+        Console.WriteLine();
+        ShowArray(sortArray, "Sorted Array");   // 顯示最後排序完畢之資料
+    }
+    
+    static void ShowArray(int[] array, string msg)
+    {
+        Console.Write($"{msg}:\n[");
+        for(int j = 0; j < array.Length ; j++)
+        {
+            Console.Write($"{array[j]}, ");
+            if(j == array.Length - 1) Console.Write("\b\b]");
+        }
+    }
+    
+    static void BubbleSort(ref int[] sourceArray)
+    {
+        // 外迴圈控制循環數
+        for(int i = 1; i < sourceArray.Length; i++)
+        {
+            // 內迴圈控制每循環內相鄰兩數比較
+            for(int j = 0; j < (sourceArray.Length -i); j++)
+            {
+                // 若左側值>右側值，兩數交換
+                if(sourceArray[j] > sourceArray[j + 1])
+                {
+                    int temp = sourceArray[j];
+                    sourceArray[j] = sourceArray[j + 1];
+                    sourceArray[j + 1] = temp;
+                }
+            }
+            ShowArray(sourceArray, $"\n第{i}次循環");   // 顯示每次大循環之結果
+        }
+    }
+}
---
+++ Console Apps/SelectionSortPresentation/SelectionSort.cs
@@ -0,0 +1,73 @@
+﻿namespace SelectionSortPresentation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int ArraySize = 7;
+
+        int[] oriArray = new int[ArraySize];
+        Console.WriteLine($"Input {ArraySize} numbers:");
+
+        // 使用者輸入原始陣列資料(int)
+        for(int i = 0; i < ArraySize;)
+        {
+            // 限制使用者僅能輸入整數數字
+            if(int.TryParse(Console.ReadLine(),out int k))
+            {
+                oriArray[i] = k;
+                i++;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
+        Console.WriteLine();
+        ShowArray(oriArray, "Original Array");  // 顯示原始資料
+
+        int[] sortArray = new int[ArraySize];
+        Array.Copy(oriArray, sortArray, ArraySize); // 複製一陣列用於SelectionSort
+
+        SelectionSort_Smallest(ref sortArray);  // 選擇排序法（由小到大）
+
+        Console.WriteLine();
+        ShowArray(sortArray, "Sorted Array");   // 顯示最後排序完畢之資料
+    }
+    
+    static void ShowArray(int[] array, string msg)
+    {
+        Console.Write($"{msg}:\n[");
+        for(int j = 0; j < array.Length ; j++)
+        {
+            Console.Write($"{array[j]}, ");
+            if(j == array.Length - 1) Console.Write("\b\b]");
+        }
+    }
+    
+    static void SelectionSort_Smallest(ref int[] sourceArray)
+    {
+        // 外迴圈控制目前陣列最前端的索引值(minIndex)
+        for(int i = 0; i < (sourceArray.Length - 1); i++)
+        {
+            int minIndex = i;
+
+            // 內迴圈檢查陣列內尚未排序之最小資料的索引值
+            // 若minIndex指向的資料數值＞尚未排序之陣列範圍內最小資料，則改變minIndex
+            for(int j = i + 1; j < sourceArray.Length; j++)
+            {
+                if(sourceArray[minIndex] > sourceArray[j]) minIndex = j;
+            }
+            
+            // 若尚未排序之陣列內最小資料不在最前端，則交換位置
+            if(minIndex != i)
+            {
+                int temp = sourceArray[minIndex];
+                sourceArray[minIndex] = sourceArray[i];
+                sourceArray[i] = temp;
+            }
+
+            ShowArray(sourceArray,$"\nRound {i+1}");    // 顯示每次大循環之結果
+        }
+    }
+}
